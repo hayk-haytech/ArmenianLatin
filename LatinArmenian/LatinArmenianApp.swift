b0a529e31b3d2d5a751d06cfa26e153b6407485c
Hayk <hayk@hhcompany.be>
2021-11-09 00:02:25 +0100

    Initial Commit

---
+++ LatinArmenian/LatinArmenianApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatinArmenianApp.swift
+//  LatinArmenian
+//
+//  Created by Hayk Tamazian on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LatinArmenianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
